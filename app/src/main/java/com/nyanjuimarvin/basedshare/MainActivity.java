50f82898e827eedf366b4ed6ca05ca8a7627ef2c
marvin <marnyanjui@gmail.com>
2022-03-26 18:49:58 +0300

    Add view binding dependencies

--- app/src/main/java/com/nyanjuimarvin/basedshare/MainActivity.java
+++ app/src/main/java/com/nyanjuimarvin/basedshare/MainActivity.java
@@ -2,13 +2,30 @@ package com.nyanjuimarvin.basedshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.nyanjuimarvin.basedshare.activities.DetailActivity;
+import com.nyanjuimarvin.basedshare.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        mainBinding.detailViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
