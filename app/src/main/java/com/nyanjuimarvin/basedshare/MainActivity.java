b215a095bae89bbfc686fb4ded9e5c3e28b4887f
marvin <marnyanjui@gmail.com>
2022-03-27 06:04:02 +0300

    Add viewPager2 dependency

--- app/src/main/java/com/nyanjuimarvin/basedshare/MainActivity.java
+++ app/src/main/java/com/nyanjuimarvin/basedshare/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.nyanjuimarvin.basedshare.activities.DetailActivity;
 import com.nyanjuimarvin.basedshare.databinding.ActivityMainBinding;
@@ -12,19 +14,22 @@ import com.nyanjuimarvin.basedshare.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding mainBinding;
+     private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-
+        View view = mainBinding.getRoot();
+        setContentView(view);
+        mainBinding.detailViewButton.bringToFront();
         mainBinding.detailViewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, DetailActivity.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this,"Hey There",Toast.LENGTH_LONG).show();
+                Log.d(MainActivity.class.getSimpleName(),"Button Clicked");
             }
         });
     }
---
+++ app/src/main/java/com/nyanjuimarvin/basedshare/adapters/FragmentViewAdapter.java
@@ -0,0 +1,23 @@
+package com.nyanjuimarvin.basedshare.adapters;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentViewAdapter extends FragmentStateAdapter {
+
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
