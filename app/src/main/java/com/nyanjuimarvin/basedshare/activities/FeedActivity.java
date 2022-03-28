56c229d0d8d338ceb27a277ebb6639425a3abe1b
marvin <marnyanjui@gmail.com>
2022-03-28 06:55:10 +0300

    Display dummy data in RecyclerView

--- app/src/main/java/com/nyanjuimarvin/basedshare/activities/FeedActivity.java
+++ app/src/main/java/com/nyanjuimarvin/basedshare/activities/FeedActivity.java
@@ -1,16 +1,51 @@
 package com.nyanjuimarvin.basedshare.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.nyanjuimarvin.basedshare.R;
+import com.nyanjuimarvin.basedshare.adapters.FeedViewAdapter;
+import com.nyanjuimarvin.basedshare.databinding.ActivityFeedBinding;
+import com.nyanjuimarvin.basedshare.models.GeneralFeed;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;;
+    private List<GeneralFeed> generalFeedsList;
+    private ActivityFeedBinding feedBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+        feedBinding = ActivityFeedBinding.inflate(getLayoutInflater());
+        View view = feedBinding.getRoot();
+        setContentView(view);
+        recyclerView = feedBinding.recyclerCards;
+
+        generalFeedsList = new ArrayList<>();
+        generalFeedsList.add(new GeneralFeed("Nvidia RTX 3090 prices just dipped","Games","HackerSlasher3"));
+        generalFeedsList.add(new GeneralFeed("I have proof why Ryan Gosling is literally me","Kino","Nitwit5"));
+        generalFeedsList.add(new GeneralFeed("Doom Eternal Quick switching on Console made simpler","Game","Makyr Khan"));
+        generalFeedsList.add(new GeneralFeed("Discover the song around by Floral Shop","Music","anonmiusicus"));
+        generalFeedsList.add(new GeneralFeed("It had me saying \"Let's see Paul Allen's Card\"","Kino","ALtUser9"));
+        generalFeedsList.add(new GeneralFeed("Obi-Wan is literally the master of the low ground","Kino","maulDarth"));
+        generalFeedsList.add(new GeneralFeed("Here's to you is the greatest score from Sacco and Vanzetti","Music","kingHubris"));
+        generalFeedsList.add(new GeneralFeed("Play Metal Gear Solid V on your birthdate to get a pleasant surprise","Game","Volgin43"));
+        generalFeedsList.add(new GeneralFeed("If you save Roggvir in Solitude, he still get's a heart attack..Lmaoo","Game","RestfulDraugr"));
+
+        //Initialize Adapter
+        FeedViewAdapter feedAdapter = new FeedViewAdapter(this,generalFeedsList);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(feedAdapter);
     }
 }
