4336c8bb91712f4be8d152a65c824b6d79d24c25
marvin <marnyanjui@gmail.com>
2022-04-03 09:10:21 +0300

    Delete oAuth interface

--- app/src/main/java/com/nyanjuimarvin/basedshare/endpoints/OauthEndpoint.java
+++
@@ -1,9 +0,0 @@
-package com.nyanjuimarvin.basedshare.endpoints;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface OauthEndpoint {
-    @GET("authorize")
-    Call<String> getToken();
-}
