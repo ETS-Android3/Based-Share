4ab43b2d906e8754827de637aa9b01f6528e4136
marvin <marnyanjui@gmail.com>
2022-04-03 11:41:46 +0300

    Add film retrofit client

--- app/src/main/java/com/nyanjuimarvin/basedshare/endpoints/FilmEndpoint.java
+++ app/src/main/java/com/nyanjuimarvin/basedshare/endpoints/FilmEndpoint.java
@@ -1,9 +1,13 @@
 package com.nyanjuimarvin.basedshare.endpoints;
 
+import com.nyanjuimarvin.basedshare.models.film.FilmResponse;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface FilmEndpoint {
 
     @GET("multi")
-    Call<>getFilms(@Query(""));
+    Call<FilmResponse> getFilms(@Query("query")String query);
 }
---
+++ app/src/main/java/com/nyanjuimarvin/basedshare/retrofit/FilmClient.java
@@ -0,0 +1,41 @@
+package com.nyanjuimarvin.basedshare.retrofit;
+
+import static com.nyanjuimarvin.basedshare.constants.Constants.MOVIE_DB_BASE_URL;
+
+import com.nyanjuimarvin.basedshare.endpoints.FilmEndpoint;
+
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class FilmClient {
+
+    public static Retrofit retrofit = null;
+
+    public static FilmEndpoint getFilmClient(){
+        if(retrofit == null){
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override//Interceptor
+                        public Response intercept(Chain chain) throws IOException {
+                            Request request = chain.request().newBuilder().build();
+                            return chain.proceed(request);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(MOVIE_DB_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit.create(FilmEndpoint.class);
+    }
+}
