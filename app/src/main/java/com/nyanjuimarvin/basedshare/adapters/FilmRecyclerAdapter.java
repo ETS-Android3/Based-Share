41d4d9ac43e856dad7b0294f67b47b1cfb54dc49
marvin <marnyanjui@gmail.com>
2022-04-04 05:31:16 +0300

    Add custom ItemClickListener for game and film recyclerviews

--- app/src/main/java/com/nyanjuimarvin/basedshare/adapters/FilmRecyclerAdapter.java
+++ app/src/main/java/com/nyanjuimarvin/basedshare/adapters/FilmRecyclerAdapter.java
@@ -1,16 +1,42 @@
 package com.nyanjuimarvin.basedshare.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.nyanjuimarvin.basedshare.R;
+import com.nyanjuimarvin.basedshare.databinding.FragmentMovieCardBinding;
+import com.nyanjuimarvin.basedshare.models.film.Result;
+
+import java.util.List;
+
 public class FilmRecyclerAdapter extends RecyclerView.Adapter<FilmRecyclerAdapter.FilmViewHolder>{
+
+    private Context context;
+    private List<Result> filmResults;
+    private FilmOnClickListener listener;
+    private FragmentMovieCardBinding movieCardBinding;
+
+
+    public interface FilmOnClickListener{
+        void onItemClick(Result result);
+    }
+
+    public FilmRecyclerAdapter(Context context, List<Result> filmResults, FilmOnClickListener listener) {
+        this.context = context;
+        this.filmResults = filmResults;
+        this.listener = listener;
+    }
+
     @NonNull
     @Override
     public FilmRecyclerAdapter.FilmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_movie_card,parent,false);
+        return new FilmViewHolder(view);
     }
 
     @Override
@@ -20,13 +46,23 @@ public class FilmRecyclerAdapter extends RecyclerView.Adapter<FilmRecyclerAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return filmResults.size();
     }
 
     public static class FilmViewHolder extends RecyclerView.ViewHolder{
 
         public FilmViewHolder(@NonNull View itemView) {
             super(itemView);
+        }
+
+        //On item click
+        public void bindView(final Result result,final FilmOnClickListener listener){
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(result);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/nyanjuimarvin/basedshare/adapters/GameRecyclerAdapter.java
+++ app/src/main/java/com/nyanjuimarvin/basedshare/adapters/GameRecyclerAdapter.java
@@ -1,17 +1,42 @@
 package com.nyanjuimarvin.basedshare.adapters;
 
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.nyanjuimarvin.basedshare.R;
+import com.nyanjuimarvin.basedshare.databinding.FragmentGameCardBinding;
+import com.nyanjuimarvin.basedshare.models.game.Result;
+
+import java.util.List;
+
 public class GameRecyclerAdapter extends RecyclerView.Adapter<GameRecyclerAdapter.GameViewHolder>{
+
+    private Context context;
+    private List<Result> gameResults;
+    private GameClickListener listener;
+    private FragmentGameCardBinding gameCardBinding;
+
+    public interface GameClickListener{
+        void onItemClick(Result result);
+    }
+
+    public GameRecyclerAdapter(Context context, List<Result> gameResults, GameClickListener listener) {
+        this.context = context;
+        this.gameResults = gameResults;
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
     public GameRecyclerAdapter.GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_game_card,parent,false);
+        return new GameViewHolder(view);
     }
 
     @Override
@@ -21,13 +46,22 @@ public class GameRecyclerAdapter extends RecyclerView.Adapter<GameRecyclerAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return gameResults.size();
     }
 
     public static class GameViewHolder extends RecyclerView.ViewHolder{
 
         public GameViewHolder(@NonNull View itemView) {
             super(itemView);
+        }
+
+        public void bindView(final Result result,final GameClickListener listener){
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(result);
+                }
+            });
         }
     }
 }
