45c51833fb38d4b2adf7ed6d69864174c4e5495f
marvin <marnyanjui@gmail.com>
2022-04-04 04:49:02 +0300

    Add games and film cardview layouts

---
+++ app/src/main/java/com/nyanjuimarvin/basedshare/activities/GameResultsActivity.java
@@ -0,0 +1,16 @@
+package com.nyanjuimarvin.basedshare.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nyanjuimarvin.basedshare.R;
+
+public class GameResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_results);
+    }
+}
