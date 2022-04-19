7b63456b4455d8bc82e4063a9eac7c01cadf913e
marvin <marnyanjui@gmail.com>
2022-04-19 07:28:52 +0300

    Override swipe and longPress helpers

--- app/src/main/java/com/nyanjuimarvin/basedshare/gestures/ItemTouchHelperCallback.java
+++ app/src/main/java/com/nyanjuimarvin/basedshare/gestures/ItemTouchHelperCallback.java
@@ -9,6 +9,10 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
     //Interface
     private ItemTouchCallback itemTouchCallback;
 
+    public ItemTouchHelperCallback(ItemTouchCallback itemTouchCallback) {
+        this.itemTouchCallback = itemTouchCallback;
+    }
+
     @Override
     public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
         return 0;
@@ -22,5 +26,15 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
 
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return true;
     }
 }
